d7a57b5fe11b26922fab13f74a75b262e5e958de
FelipeMoraUAO <andres_f.mora@uao.edu.co>
2025-09-30 22:10:31 -0500

    Creacion del Player

---
+++ EcoSystem_Game_2D/Assets/Player/Códigos/Player.cs
@@ -0,0 +1,36 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [Header("Configuración del Player")]
+    public int vidas = 3;
+
+    [Header("UI - Corazones")]
+    public Image[] corazones; // arrastra aquí las imágenes de corazones (3 en tu caso)
+    
+    public void RecibirDaño(int cantidad)
+    {
+        vidas -= cantidad;
+
+        if (vidas < 0) vidas = 0; 
+
+        // 🔴 Actualiza corazones según vidas
+        for (int i = 0; i < corazones.Length; i++)
+        {
+            if (i < vidas)
+                corazones[i].gameObject.SetActive(true); // Corazón visible
+            else
+                corazones[i].gameObject.SetActive(false); // Corazón eliminado
+        }
+
+        Debug.Log("Recibió daño, le quedan " + vidas + " vidas.");
+
+        if (vidas <= 0)
+        {
+            Debug.Log("¡El jugador ha muerto! Cargando Game Over...");
+            SceneManager.LoadScene("Game Over"); // 👈 Asegúrate que exista esa escena
+        }
+    }
+}
